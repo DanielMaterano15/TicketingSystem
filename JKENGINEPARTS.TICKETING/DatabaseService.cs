5ecfed8d8f948b2e7ea3e96bc5e0aa153cf2cb81
DanielMaterano15 <materanodaniel.15@gmail.com>
2026-07-07 16:52:37 -0400

    Implementacion de la DB

--- JKENGINEPARTS.TICKETING/DatabaseService.cs
+++ JKENGINEPARTS.TICKETING/DatabaseService.cs
@@ -1,6 +1,30 @@
-﻿namespace JKENGINEPARTS.TICKETING;
+﻿using System;
+using System.Collections.Generic;
+using Npgsql;
+using JKENGINEPARTS.TICKETING.Models;
 
-public class DatabaseService
+namespace JKENGINEPARTS.TICKETING
 {
-    
+    public class DatabaseService
+    {
+        private readonly string _connectionString = "Host=db.zruiaflceoyvcfnnujjp.supabase.co;" +
+                                                    "Port=5432;" + 
+                                                    "Database=postgres;" + 
+                                                    "Username=postgres;" + 
+                                                    "Password=C59VrAGER0kwLa2K;" + 
+                                                    "SSL Mode=Require;" + 
+                                                    "Trust Server Certificate=true";
+
+        public List<Ticket> ObtenerTickets()
+        {
+            var lista = new List<Ticket>();
+
+            using (var conn = new NpgsqlConnection(_connectionString))
+            {
+                conn.Open();
+                string query = "SELECT id, taller_nombre, vehiculo_info, status FROM ticekts ORDER BY "
+            }
+        }
+    }
+
 }
--- JKENGINEPARTS.TICKETING/obj/Debug/net9.0-windows/JKENGINEPARTS.TICKETING.AssemblyInfo.cs
+++ JKENGINEPARTS.TICKETING/obj/Debug/net9.0-windows/JKENGINEPARTS.TICKETING.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JKENGINEPARTS.TICKETING")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9205edfa901fc318abd98de6b8cf075fe96b600")]
 [assembly: System.Reflection.AssemblyProductAttribute("JKENGINEPARTS.TICKETING")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JKENGINEPARTS.TICKETING")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
